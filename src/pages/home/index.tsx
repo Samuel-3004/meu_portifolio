690c916a3449f7bf810a24cdc06751b85767577e
Samuel-3004 <samuelszportela@hotmail.com>
2023-07-24 21:47:33 -0300

    fix: include link portfolio

--- src/pages/home/index.tsx
+++ src/pages/home/index.tsx
@@ -30,8 +30,7 @@ import {
 
 export const Home = (): JSX.Element => {
   const gihubUrl = `https://github.com/${userData.githubUser}`;
-  const portfolioUrl = `https://github.com/${userData.githubUser}/my-portfolio`;
-
+  const portfolioUrl = `https://github.com/Samuel-3004/meu_portifolio`;
   return (
     <main id="home">
       <Header>
